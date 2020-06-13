b19c142c1888a5c0e4f605c7c6979d8f6aa31f79
Carwin Young <carwin@mobomo.com>
2020-06-13 00:55:26 -0500

    Initial commit. Basically just styled some tables.

---
+++ js/example.js
@@ -0,0 +1,11 @@
+(function ($) {
+
+  'use strict';
+
+  Drupal.behaviors.larkCheckboxes = {
+    attach: function (context, settings) {
+      // Do JavaScript stuff here.
+    }
+  }
+
+})(jQuery);
